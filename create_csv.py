d9189acb66390f45dcc8450d6a58965892857582
alvymiamaharani <142475313+alvymiamaharani@users.noreply.github.com>
2023-12-03 11:55:30 +0800

    update cretae csv

--- create_csv.py
+++ create_csv.py
@@ -11,26 +11,27 @@ def xml_to_csv(path):
     for xml_file in glob.glob(path + '/*.xml'):
         tree = ET.parse(xml_file)
         root = tree.getroot()
-        for member in root.findall('object'):
-            value = (root.find('filename').text,
-                     int(root.find('size')[0].text),
-                     int(root.find('size')[1].text),
-                     member[0].text,
-                     int(member[4][0].text),
-                     int(member[4][1].text),
-                     int(member[4][2].text),
-                     int(member[4][3].text)
-                     )
-            xml_list.append(value)
+
+        filename = root.find('filename').text
+        width = int(root.find('size/width').text)
+        height = int(root.find('size/height').text)
+        class_name = root.find('object/name').text
+        xmin = int(root.find('object/bndbox/xmin').text)
+        xmax = int(root.find('object/bndbox/xmax').text)
+        ymin = int(root.find('object/bndbox/ymin').text)
+        ymax = int(root.find('object/bndbox/ymax').text)
+
+        xml_list.append((filename, width, height, class_name, xmin, ymin, xmax, ymax))
+
     column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
     xml_df = pd.DataFrame(xml_list, columns=column_name)
     return xml_df
 
 def main():
-    for folder in ['train','valid']:
-        image_path = os.path.join(os.getcwd(), ('datasets/' + folder))
+    for folder in ['train', 'valid']:
+        image_path = os.path.join(os.getcwd(), ('images/' + folder))
         xml_df = xml_to_csv(image_path)
-        xml_df.to_csv(('datasets/' + folder + '_labels.csv'), index=None)
+        xml_df.to_csv(('images/' + folder + '_labels.csv'), index=None)
         print('Successfully converted xml to csv.')
 
 main()
